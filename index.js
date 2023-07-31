6f816a326a426acf163c5dc31724b6c46c88a8ac
davlatov-muhriddin <muhriddind95@gmail.com>
2023-07-31 20:27:58 +0500

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Main Page");
 });
